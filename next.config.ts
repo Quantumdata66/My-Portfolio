9e9e5fd0d354b13dc584520a70306196d7793e05
Nurudeen <nurdul4002@gmail.com>
2026-07-26 14:15:52 +0100

    ci: configure Next.js static export & GitHub Actions deployment workflow for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubActions ? "/My-Portfolio" : "",
+  assetPrefix: isGithubActions ? "/My-Portfolio/" : "",
 };
 
 export default nextConfig;
